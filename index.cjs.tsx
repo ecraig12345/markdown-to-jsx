66fe19484e79357d7ddbcba697419425617137d8
Elizabeth Craig <elcraig@microsoft.com>
2022-03-11 14:18:33 -0800

    feat: ensure Markdown is set as default export even in cjs

--- index.cjs.tsx
+++ index.cjs.tsx
@@ -1,3 +1,6 @@
 import Markdown, { compiler } from './'
-Object.assign(Markdown, { compiler })
-export default Markdown as typeof Markdown & { compiler: typeof compiler }
+// The types declare a default export, so ensure that's set even in cjs
+Object.assign(Markdown, { compiler, default: Markdown })
+export default Markdown as typeof Markdown & { compiler: typeof compiler } & {
+  default: typeof Markdown
+}
